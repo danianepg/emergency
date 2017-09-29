94a9ff2a03f3a027419b92490b73491255cb8149
Daniane P. Gomes <daniane.gomes@totvs.com.br>
2017-09-29 10:26:44 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+function initMap() {
+
+	var hospitalUnimed = {
+		info: '<strong>Hospital Unimed</strong><br>\
+			   Tempo de espera: 15 minutos',
+		lat: -29.164730,
+		long: -51.200866
+	};
+
+	var hospitalCirculo = {
+		info: '<strong>Hospital do Círculo</strong> <br> \
+		       Tempo de espera: 10 minutos',
+		lat: -29.153545,
+		long: -51.173444
+	};
+
+	var hospitalSaude = {
+		info: '<strong>Hospital Saúde</strong> <br> \
+			   Tempo de espera: 20 minutos',
+		lat: -29.164720,
+		long: -51.183480
+	};
+
+	var locations = [
+      [hospitalUnimed.info, hospitalUnimed.lat, hospitalUnimed.long, 0],
+      [hospitalCirculo.info, hospitalCirculo.lat, hospitalCirculo.long, 1],
+      [hospitalSaude.info, hospitalSaude.lat, hospitalSaude.long, 2],
+    ];
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 13,
+		center: new google.maps.LatLng(-29.163403, -51.179668),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
+	var infowindow = new google.maps.InfoWindow({});
+
+	var marker, i;
+
+	for (i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
+		});
+
+		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+			return function () {
+				infowindow.setContent(locations[i][0]);
+				infowindow.open(map, marker);
+			}
+		})(marker, i));
+	}
+}
